7d6f92440b16b0a6860f372cd99cb092741703c9
KevinSchroetter <Kevin.Schroetter@Student.Reutlingen-University.de>
2017-04-24 17:39:20 +0200

    one more test

--- app.js
+++ app.js
@@ -56,7 +56,7 @@ app.use (function (req, res, next) {
                 res.redirect('https://' + req.headers.host + req.url);
         }
 });
-*/
+
 app.use(function(req,res,next){
 	var schema = req.headers["x-forwarded-proto"];
 	if(schema === "https"){
@@ -64,7 +64,7 @@ app.use(function(req,res,next){
 	}
 	next();
 });
-
+*/
 var io = require('socket.io').listen(server);
 
 var router = express.Router();
