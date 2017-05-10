13a3b16cceb9d5243221ee74a598aadddd5c6e1c
KevinSchroetter <Kevin.Schroetter@Student.Reutlingen-University.de>
2017-05-10 15:40:21 +0200

    ok

--- app.js
+++ app.js
@@ -59,14 +59,6 @@ app.use(helmet.xssFilter());
 
 
 var io = require('socket.io').listen(server);
-
-
-	
-/*
-var redis = require("redis");
-var sub = redis.createClient(), pub = redis.createClient();
-
-});*/
 var router = express.Router();
 /*
  * Cloudant information and credentials
